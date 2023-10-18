1d4a12821978b7752854ca4ea88c20c2345a1784
Michael Spencer <ms53150@gmail.com>
2023-10-17 21:25:56 -0400

    add connection to error tracker db and add function to get all users

--- server/src/models/users.model.js
+++ server/src/models/users.model.js
@@ -1,11 +1,39 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	host: process.env.DB_HOST,
+	database: process.env.DB_DATABASE,
+	port: process.env.DB_PORT,
+});
+
 async function getAllUsers() {
 	try {
 		const client = await pool.connect();
-		const query = "SELECT * FROM users";
+		const query = 'SELECT * FROM "user"';
 		const result = await client.query(query);
 		const users = result.rows;
 		client.release();
 		return users;
+	} catch (error) {
+		console.error("Error executing query", error);
+		throw error;
+	}
+}
+
+async function addNewUser() {
+	try {
+		const { first_name, last_name, email, date_of_birth } = req.body;
+		const client = await pool.connect();
+		const query =
+			"INSERT INTO student (first_name, last_name, email, date_of_birth) VALUES ($1, $2, $3, $4::DATE) RETURNING *";
+		const values = [fname, lname, email, dob];
+		const result = await client.query(query, values);
+		const insertedStudent = result.rows[0];
+		client.release();
+
+		return insertedStudent;
 	} catch (error) {
 		console.error("Error executing query", error);
 		throw error;
